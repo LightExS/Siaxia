d5b24dd2cd9ac17d766751992b224e66df609baa
Dmytro Melnyk <dimon.melnik.c@gmail.com>
2024-01-07 00:50:05 +0200

    removed faculty from options

--- telegram_commands/timetable.py
+++ telegram_commands/timetable.py
@@ -8,17 +8,11 @@ from telegram.ext import (
 )
 import timetable_puller
 
-FACULTY, GROUP, DATE, RESULT = range(4)
+GROUP, DATE, RESULT = range(3)
 
 
 async def timetable_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Оберіть факультет")
-    return FACULTY
-
-
-async def faculty_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Оберіть групу")
-    context.user_data["faculty"] = update.message.text
     return GROUP
 
 
@@ -30,26 +24,21 @@ async def group_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def date_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data["date"] = update.message.text
-    faculty = context.user_data["faculty"]
     group = context.user_data["group"]
     date = context.user_data["date"]
     await update.message.reply_text(
-        f"Факультет: {faculty}\nГрупа: {group}\nДата: {date}\nВірно?"
+        f"Факультет: \nГрупа: {group}\nДата: {date}\nВірно?"
     )
     return RESULT
 
 
 async def result_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.text == "Так":
-        faculty = context.user_data["faculty"]
         group = context.user_data["group"]
         date = context.user_data["date"]
 
-        timetables, dates = timetable_puller.get_timetable(
-            faculty=faculty, group=group, sdate=date
-        )
+        timetables, dates = timetable_puller.get_timetable(group=group, sdate=date)
 
-        # Temporary output, to be changed
         response = ""
         for table, date in zip(timetables, dates):
             response += date + "\n\n" + timetable_puller.change_table_to_str(table)
@@ -66,7 +55,6 @@ def get_handler():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("timetable", timetable_command)],
         states={
-            FACULTY: [MessageHandler(filters.TEXT, faculty_command)],
             GROUP: [MessageHandler(filters.TEXT, group_command)],
             DATE: [MessageHandler(filters.TEXT, date_command)],
             RESULT: [MessageHandler(filters.TEXT, result_command)],
