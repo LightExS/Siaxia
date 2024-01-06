09ea2ed2703fd022644094b2ae96b55156e84d11
Dmytro Melnyk <dimon.melnik.c@gmail.com>
2024-01-07 00:53:23 +0200

    Added 2 new commands, removed unnecessary imports, removed db table and made changes to support it

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,66 +1,21 @@
 # fmt:off
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
-import timetable_puller
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from private_variables import TOKEN, BOT_USERNAME
 
 import telegram_commands.timetable
 import telegram_commands.bind
+import telegram_commands.today
+import telegram_commands.tomorrow
 
 import sqlalchemy as db
 
-buttons = [[InlineKeyboardButton("btn1", callback_data="1")],[InlineKeyboardButton("btn2", callback_data="2")]]
 FACULTY, GROUP, DATE, RESULT = range(4)
 
 #commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text = f"ID: {update.effective_chat.id}",reply_markup = InlineKeyboardMarkup(buttons))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = f"ID: {update.effective_chat.id}")
     
-
-async def timetable_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Оберіть факультет")
-    return FACULTY
-
-async def faculty_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Оберіть групу")
-    context.user_data['faculty'] = update.message.text
-    return GROUP
-
-async def group_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Оберіть з якого числа")
-    context.user_data['group'] = update.message.text
-    return DATE
-
-async def date_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    context.user_data['date'] = update.message.text
-    faculty = context.user_data['faculty']
-    group = context.user_data['group']
-    date = context.user_data['date']
-    await update.message.reply_text(f"Факультет: {faculty}\nГрупа: {group}\nДата: {date}\nВірно?")
-    return RESULT
-
-async def result_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.text == "Так":
-        faculty = context.user_data['faculty']
-        group = context.user_data['group']
-        date = context.user_data['date']
-
-        
-        timetables, dates = timetable_puller.get_timetable(faculty=faculty, group=group, sdate=date)
-
-        #Temporary output, to be changed
-        response = "" 
-        for table, date in zip(timetables,dates):
-            response += date +"\n\n"+ timetable_puller.change_table_to_str(table)
-        
-        await update.message.reply_text(response)
-        return -1
-    elif update.message.text == "Ні":
-        return -1
-    else:
-        return RESULT
-
-
 def hande_response(text:str) ->str:
     if 'hello' in text:
         return "Hi!"
@@ -95,19 +50,21 @@ if __name__ == '__main__':
     connection = engine.connect()
     metadata = db.MetaData()
     user_data = db.Table("user_data", metadata, autoload_with=engine)
-    chat_data = db.Table("group_data", metadata, autoload_with=engine)
     
     print("Starting bot...")
     app = Application.builder().token(TOKEN).build()
     
     #Commands handlers
     app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(telegram_commands.today.get_handler(connection, user_data))
+    
+    app.add_handler(telegram_commands.tomorrow.get_handler(connection, user_data))
 
     #Conversation handlers
     timetable_conv_handler = telegram_commands.timetable.get_handler()
     app.add_handler(timetable_conv_handler)
     
-    bind_conv_handler = telegram_commands.bind.get_handler(connection, user_data, chat_data)
+    bind_conv_handler = telegram_commands.bind.get_handler(connection, user_data)
     app.add_handler(bind_conv_handler)
     
     
